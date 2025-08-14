0df0f207b9f4bbfa55a16ceea5b9717df686df6e
KarwaN001 <karwanusf1@gmail.com>
2025-08-14 07:29:04 -0700

    Initial Commit

---
+++ H4ker news/H4ker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4ker_newsApp.swift
+//  H4ker news
+//
+//  Created by karwan Syborg on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct H4ker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
